d8a6f30907984d2269cf9eb3578c20b096be3f01
Sudheesh Singanamalla <sudheesh@cs.washington.edu>
2023-01-25 16:19:38 -0700

    Cleanup, fetch from apt instead of mirror
    Signed-off-by: Sudheesh Singanamalla <sudheesh@cs.washington.edu>

--- install.sh
+++ install.sh
@@ -13,28 +13,6 @@ fi
 
 RELEASE="$(. /etc/os-release ; echo $UBUNTU_CODENAME)"
 
-REPO="powder"
-
-# WTF! The tabs before priority actually matter.
-echo "http://boss/mirror/repos.emulab.net/$REPO/ubuntu	priority:1" | sudo tee -a /etc/apt/emulab-$REPO-mirrorlist.txt &&
-    echo "http://repos.emulab.net/$REPO/ubuntu	priority:2" | sudo tee -a /etc/apt/emulab-$REPO-mirrorlist.txt &&
-    echo "deb mirror+file:/etc/apt/emulab-$REPO-mirrorlist.txt $RELEASE main" | sudo tee -a /etc/apt/sources.list.d/$REPO.list
-if [ $? -ne 0 ]; then
-    echo "creating $REPO failed"
-    exit 1
-fi
-
-REPO="powder-endpoints"
-
-# WTF! The tabs before priority actually matter.
-echo "http://boss/mirror/repos.emulab.net/$REPO/ubuntu	priority:1" | sudo tee -a /etc/apt/emulab-$REPO-mirrorlist.txt &&
-    echo "http://repos.emulab.net/$REPO/ubuntu	priority:2" | sudo tee -a /etc/apt/emulab-$REPO-mirrorlist.txt &&
-    echo "deb mirror+file:/etc/apt/emulab-$REPO-mirrorlist.txt $RELEASE main" | sudo tee -a /etc/apt/sources.list.d/$REPO.list
-if [ $? -ne 0 ]; then
-    echo "creating $REPO failed"
-    exit 1g
-fi
-
 sudo apt-get update -y
 if [ $? -ne 0 ]; then
     echo 'apt-get update failed'
@@ -62,14 +40,6 @@ fi
 cd /usr/lib/x86_64-linux-gnu/
 sudo ln -s liborc-0.4.so.0.28.0 liborc-0.4.so
 
-#
-# This sets the X11 geometry for uhd_fft. It does not take a --geometry arg.
-#
-GENIUSER=`geni-get user_urn | awk -F+ '{print $4}'`
-HOMEDIR="/users/$GENIUSER"
-sudo -u $GENIUSER tar xf /local/repository/uhd_fft-config.tar -C $HOMEDIR
-
-#
 # Marker that says we completed the install.
 #
 sudo touch /etc/.txready
